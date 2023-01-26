f0cee2fe8e4bbfdeb153ee6536ba31386c3bdc13
aman-maharshi <amanm4harshi@gmail.com>
2023-01-26 15:51:23 +0530

    init

---
+++ simple-blog/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className="p-4">
+            <h2 className="text-2xl font-bold">Lorem, ipsum dolor.</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+                at.
+            </p>
+        </div>
+    )
+}
