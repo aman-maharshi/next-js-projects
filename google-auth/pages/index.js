b4d9e3d49b299290db392fe5e8442884295b8292
aman-maharshi <amanm4harshi@gmail.com>
2023-01-27 20:47:05 +0530

    init

---
+++ google-auth/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Login with Google</title>
+            </Head>
+            <div className="p-4">
+                <h2 className="font-bold text-2xl">Lorem, ipsum dolor.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Ut, nobis? Accusantium, quam beatae. Ex pariatur error ipsum
+                    repellat libero mollitia?
+                </p>
+            </div>
+        </div>
+    )
+}
